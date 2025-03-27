bdb06deeae2e68429e344b9114f9e1884be86034
Isha Ingle <ingle.isha@gmail.com>
2025-03-27 14:16:07 +0530

    Implemented specs for string calculator

---
+++ string_calculator_spec.rb
@@ -0,0 +1,32 @@
+require 'rspec'
+require_relative 'string_calculator'
+
+RSpec.describe 'StringCalculator' do
+	describe '#add' do
+		let(:calculator) { StringCalculator.new }
+
+		it "returns 0 for an empty string" do
+			expect(calculator.add("")).to eq(0)
+		end
+
+		it "returns the number itself for a single number" do
+			expect(calculator.add("1")).to eq(1)
+		end
+
+		it "returns the sum of two numbers separated by a comma" do
+			expect(calculator.add("1,5")).to eq(6)
+		end
+
+		it "returns the sum of multiple numbers separated by new lines" do
+			expect(calculator.add("1\n2,3")).to eq(6)
+		end
+
+		it "returns the sum of numbers separated by a custom delimiter" do
+			expect(calculator.add("//;\n1;2")).to eq(3)
+		end
+
+		it "throws an exception for negative numbers" do
+			expect { calculator.add("-2,3,-4")}.to raise_error(CustomError, "negative numbers not allowed: -2, -4")
+		end
+	end
+end
